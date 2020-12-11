f8b7f2726f7bd3d49235a204eee88efefaeff114
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2020-12-11 14:03:44 -0700

    flex buttons on landing

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,18 +1,38 @@
 import React from 'react';
 import './Landing.css';
-import { Fab } from '@material-ui/core/Fab/Fab';
 import Button from '@material-ui/core/Button';
+import { Link } from 'react-router-dom';
 
 
 export default function Landing() {
     return (
         <div className='landing-container'>
             <div className='logo-container'>
-                <img className='logo' src='/wwc-white-logo.png' alt='Women Who Climb Logo'/>
+                <img 
+                    className='logo' 
+                    src='/wwc-white-logo.png' 
+                    alt='Women Who Climb Logo'
+                />
             </div>
             <div className='button-container'>
-                    <Button variant='contained' color='secondary'>Sign in</Button>
-                    <Button variant='contained' color='secondary'>Register</Button>
+                    <Button 
+                        variant='contained' 
+                        color='secondary' 
+                        aria-label="login"
+                        component={ Link } 
+                        to='/login'
+                    >
+                        Login
+                    </Button>
+                    <Button 
+                        variant='contained' 
+                        color='secondary' 
+                        aria-label="register"
+                        component={ Link } 
+                        to='/register'
+                    >
+                        Register
+                    </Button>
             </div>
         </div>
     )
